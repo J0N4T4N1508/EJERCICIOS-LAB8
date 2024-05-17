9ac57d05dd5bef2dbdf8155c0e0d5e6cab3fdafd
J0N4T4N1508 <167550570+J0N4T4N1508@users.noreply.github.com>
2024-05-17 03:01:08 -0500

    Add files via upload

---
+++ EJERCICIO-06/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Escriba una función que reciba dos conjuntos de números y devuelva un conjunto con los números que
+están en el segundo conjunto pero no en el primero. */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<int> conjuntoUno = new HashSet<int> { 1, 2, 3, 4, 5, 6 };
+        HashSet<int> conjuntoDos = new HashSet<int> { 5, 6, 7, 8, 9, 10 };
+
+        HashSet<int> diferencia = DiferenciaConjuntos(conjuntoUno, conjuntoDos);
+
+        Console.WriteLine("Diferencia de conjuntos:");
+        foreach (var numero in diferencia)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+
+    static HashSet<int> DiferenciaConjuntos(HashSet<int> conjuntoUno, HashSet<int> conjuntoDos)
+    {
+        conjuntoDos.ExceptWith(conjuntoUno);
+        return conjuntoDos;
+    }
+}
