260c0341ecfbd192c956d1d76aeec11c0d884c35
J0N4T4N1508 <167550570+J0N4T4N1508@users.noreply.github.com>
2024-05-17 03:03:55 -0500

    Add files via upload

---
+++ EJERCICIO-10/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Escriba una función que reciba un conjunto de palabras y devuelva un conjunto con las palabras que
+contienen una letra determinada*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<string> conjuntoPalabras = new HashSet<string> { "hola", "manzana", "sistemas", "azul", "marron", "lapiz" };
+        char letra = 'o';
+
+        Console.WriteLine($"Palabras que contienen la letra {letra}: ");
+        PalabrasConLetraDeterminada(conjuntoPalabras, letra);
+    }
+
+    static void PalabrasConLetraDeterminada(HashSet<string> palabras, char letra)
+    {
+        foreach (string palabra in palabras)
+        {
+            if (palabra.Contains(letra))
+            {
+                Console.WriteLine(palabra);
+            }
+        }
+    }
+}
