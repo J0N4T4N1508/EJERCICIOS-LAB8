2929a9c0aef14a0cb2267db7cb2cbb2018490697
J0N4T4N1508 <167550570+J0N4T4N1508@users.noreply.github.com>
2024-05-17 03:01:56 -0500

    Add files via upload

---
+++ EJERCICIO-08/Program.cs
@@ -0,0 +1,57 @@
+﻿/*Escriba una función que reciba un conjunto de palabras y devuelva un conjunto con las palabras que son
+palíndromos. */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<string> conjuntoPalabras = new HashSet<string> { "estructuradedatos","radar", "oso", "reconocer", "casa", "sistemas", "ana", "solos" };
+
+        HashSet<string> palindromos = EncontrarPalindromos(conjuntoPalabras);
+
+        Console.WriteLine("Palabras en el conjunto:");
+        foreach (var palabra in conjuntoPalabras)
+        {
+            Console.WriteLine(palabra);
+        }
+
+        Console.WriteLine("\nPalabras que son palíndromos:");
+        foreach (var palindromo in palindromos)
+        {
+            Console.WriteLine(palindromo);
+        }
+    }
+
+    static HashSet<string> EncontrarPalindromos(HashSet<string> conjuntoPalabras)
+    {
+        HashSet<string> palindromos = new HashSet<string>();
+
+        foreach (string palabra in conjuntoPalabras)
+        {
+            if (EsPalindromo(palabra))
+            {
+                palindromos.Add(palabra);
+            }
+        }
+
+        return palindromos;
+    }
+
+    static bool EsPalindromo(string palabra)
+    {
+        int i = 0;
+        int j = palabra.Length - 1;
+
+        while (i < j)
+        {
+            if (palabra[i] != palabra[j])
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
