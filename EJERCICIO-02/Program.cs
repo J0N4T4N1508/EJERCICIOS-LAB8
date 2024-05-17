d504db6241b170d6c1fd0aa764aac319c9417bd1
J0N4T4N1508 <167550570+J0N4T4N1508@users.noreply.github.com>
2024-05-17 02:59:08 -0500

    Add files via upload

---
+++ EJERCICIO-02/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Escriba una función que reciba un conjunto de palabras y devuelva un conjunto con las palabras que comienzan con una letra determinada. */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var palabras = new List<string> { "sistemas", "programacion", "consola", "universidad" ,"perro"};
+        char letraDeterminada = 'p'; // Especifica la letra determinada
+
+        var resultado = ConjuntoPalabras(palabras, letraDeterminada);
+
+        Console.WriteLine("Palabras que comienzan con la letra '" + letraDeterminada + "':");
+        foreach (var palabra in resultado)
+        {
+            Console.WriteLine(palabra);
+        }
+    }
+
+    static List<string> ConjuntoPalabras(List<string> palabras, char letra)
+    {
+        var resultado = new List<string>();
+
+        foreach (var palabra in palabras)
+        {
+            if (!string.IsNullOrEmpty(palabra) && palabra[0] == letra)
+            {
+                resultado.Add(palabra);
+            }
+        }
+
+        return resultado;
+    }
+}
