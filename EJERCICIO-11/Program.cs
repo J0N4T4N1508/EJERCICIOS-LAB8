e103f670a0d3e1269d6a51c1416d4052563d2bbb
J0N4T4N1508 <167550570+J0N4T4N1508@users.noreply.github.com>
2024-05-17 03:04:45 -0500

    Add files via upload

---
+++ EJERCICIO-11/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Escriba una función que reciba un conjunto de números y devuelva un conjunto con los números que
+están ordenados de menor a mayor. */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<int> conjunto = new HashSet<int> { 1, 5, 2, 6, 4, 3 };
+        HashSet<int> conjuntoOrdenado = ConjuntoOrdenado(conjunto);
+
+        Console.WriteLine("Conjunto ordenado:");
+        foreach (var numero in conjuntoOrdenado)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+
+    static HashSet<int> ConjuntoOrdenado(HashSet<int> conjunto)
+    {
+        List<int> listaOrdenada = conjunto.ToList();
+        listaOrdenada.Sort();
+
+        return new HashSet<int>(listaOrdenada);
+    }
+}
