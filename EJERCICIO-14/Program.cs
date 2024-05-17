cedced862ee95849542294dd200feba5cf6289bc
J0N4T4N1508 <167550570+J0N4T4N1508@users.noreply.github.com>
2024-05-17 03:07:08 -0500

    Add files via upload

---
+++ EJERCICIO-14/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Escriba una función que reciba un conjunto de números y devuelva un conjunto con los números que no
+están duplicados. */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numeros = new List<int> { 1, 1, 2, 3, 4, 5, 4, 8, 2, 7, 9 };
+        HashSet<int> noDuplicados = NoDuplicados(numeros);
+
+        Console.WriteLine("Números no duplicados:");
+        foreach (var numero in noDuplicados)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+
+    static HashSet<int> NoDuplicados(List<int> conjunto)
+    {
+        HashSet<int> noDuplicados = new HashSet<int>();
+
+        for (int i = 0; i < conjunto.Count; i++)
+        {
+            int contar = 0;
+            for (int j = 0; j < conjunto.Count; j++)
+            {
+                if (conjunto[i] == conjunto[j])
+                {
+                    contar++;
+                }
+            }
+            if (contar == 1)
+            {
+                noDuplicados.Add(conjunto[i]);
+            }
+        }
+
+        return noDuplicados;
+    }
+}
