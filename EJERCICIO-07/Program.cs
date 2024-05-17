83c4f91ee66f7ade4e3cb75d6a7870f9b68efad3
J0N4T4N1508 <167550570+J0N4T4N1508@users.noreply.github.com>
2024-05-17 03:01:33 -0500

    Add files via upload

---
+++ EJERCICIO-07/Program.cs
@@ -0,0 +1,57 @@
+﻿/*Escriba una función que reciba un conjunto de palabras y devuelva un conjunto con las palabras que son
+anagramas. */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<string> conjuntoPalabrasEjemplo = new HashSet<string> { "listen", "silent", "enlist", "hello", "world", "python" };
+
+        HashSet<string> resultadoAnagramas = EncontrarAnagramas(conjuntoPalabrasEjemplo);
+
+        Console.WriteLine("Conjunto de palabras:");
+        foreach (var palabra in conjuntoPalabrasEjemplo)
+        {
+            Console.WriteLine(palabra);
+        }
+
+        Console.WriteLine("\nConjunto de anagramas:");
+        foreach (var anagrama in resultadoAnagramas)
+        {
+            Console.WriteLine(anagrama);
+        }
+    }
+
+    static HashSet<string> EncontrarAnagramas(HashSet<string> conjuntoPalabras)
+    {
+        HashSet<string> anagramas = new HashSet<string>();
+
+        foreach (string palabra1 in conjuntoPalabras)
+        {
+            foreach (string palabra2 in conjuntoPalabras)
+            {
+                if (palabra1 != palabra2 && SonAnagramas(palabra1, palabra2))
+                {
+                    anagramas.Add(palabra1);
+                    anagramas.Add(palabra2);
+                }
+            }
+        }
+
+        return anagramas;
+    }
+
+    static bool SonAnagramas(string palabra1, string palabra2)
+    {
+        if (palabra1.Length != palabra2.Length)
+            return false;
+
+        var arrayPalabra1 = palabra1.ToCharArray();
+        var arrayPalabra2 = palabra2.ToCharArray();
+
+        Array.Sort(arrayPalabra1);
+        Array.Sort(arrayPalabra2);
+
+        return arrayPalabra1.SequenceEqual(arrayPalabra2);
+    }
+}
